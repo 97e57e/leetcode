8d61c98e54e35784d1dc1e8a1ab4f316cd5b79d8
GardenLee <khjvvv7@gmail.com>
2020-04-13 17:45:06 +0900

    ReverseLinkedLists
    다시 해보기

---
+++ LinkedList/ReverseLinkedLists.java
@@ -0,0 +1,34 @@
+package leetcode.linkedList;
+
+public class ReverseLinkedLists {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+
+        printNode(l1);
+        ListNode head = reverseList(l1);
+        printNode(head);
+    }
+    public static ListNode reverseList(ListNode current) {
+        ListNode prev = null;
+        ListNode next = null;
+        while(current !=null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+
+
+    public static void printNode(ListNode head) {
+        while(head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
